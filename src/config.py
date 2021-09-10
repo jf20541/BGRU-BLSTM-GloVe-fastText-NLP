fd6371525b2f4f0b03952847431066e7cb7de5e4
jf20541 <69116513+jf20541@users.noreply.github.com>
2021-09-10 13:46:20 -0400

    Update config.py

--- src/config.py
+++ src/config.py
@@ -1,10 +1,13 @@
+import torch
+
 TRAINING_FILE = "../inputs/train.csv"
 TRAINING_FILE_CLEAN = "../inputs/clean_train.csv"
-TRAINING_FILE_CLEAN_FOLDS = "../inputs/train_clean_folds.csv"
 MODEL_PATH = "../models/imdb_model.bin"
 GLOVE_PARAMS = "../models/glove.6B.100d.txt"
+FASTTEXT_PARAMS = "../models/wiki.simple.vec"
 TRAIN_BATCH_SIZE = 75
 TEST_BATCH_SIZE = 50
 MAX_LENGTH = 256
 EPOCHS = 20
 LEARNING_RATE = 0.0003
+DEVICE = torch.device("cuda")
