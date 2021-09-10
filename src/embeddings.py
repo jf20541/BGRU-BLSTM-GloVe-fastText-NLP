080789608df3874d4812112b3565cde60a30a021
jf20541 <69116513+jf20541@users.noreply.github.com>
2021-09-10 13:45:08 -0400

    Update embeddings.py

--- src/embeddings.py
+++ src/embeddings.py
@@ -29,3 +29,32 @@ class GloVeEmbedding:
         return self.create_embedding_matrix(
             tokenizer.word_index, embedding_dict=glove_embedding
         )
+
+
+class FastTextEmbedding:
+    def __init__(self, dataframe, fasttext_params):
+        self.dataframe = dataframe
+        self.fast = fasttext_params
+
+    def create_embedding_matrix(self, word_index, embedding_dict=None, d_model=300):
+        """Creates the embedding matrix save in numpy array
+        Args:
+            word_index (dict): dictionary with tokens
+            embedding_dict (dict, optional): dict with word embedding
+            d_model (int): dimension of word pretrained embedding (Defaults to 300) 
+        Returns:
+            [array]: array with embedding vectors for all known words
+        """
+        embedding_matrix = np.zeros((len(word_index) + 1, d_model))
+        for word, index in word_index.items():
+            if word in embedding_dict:
+                embedding_matrix[index] = embedding_dict[word]
+        return embedding_matrix
+
+    def embedding_matrix(self, fasttext_embedding):
+        # tokenize review words
+        tokenizer = Tokenizer()
+        tokenizer.fit_on_texts(self.dataframe.values.tolist())
+        return self.create_embedding_matrix(
+            tokenizer.word_index, embedding_dict=fasttext_embedding
+        )
